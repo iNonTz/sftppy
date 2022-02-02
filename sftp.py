557fd770273469c5409d9b0d055925cc0f224db8
iNonTz <SmirNoffIRc@gmail.com>
2022-02-02 08:03:23 +0700

    Update to current version

--- sftp.py
+++ sftp.py
@@ -6,40 +6,62 @@ from os import walk
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-config_file = 'config.json'
+config_normal_server_file = 'config-nonjex-crawler.json'
+config_jex_server_file = 'config-jex-crawler.json'
 private_key = 'privateKeyForCrawler.pem'
-remote_path = '/home/'
+remote_path = '/home/crawler/app/'
 localpath =  ROOT_DIR + '\\Paste_File_Here\\'
+NormalCrawlerPath =  ROOT_DIR + '\\NormalCrawler\\'
+JEXCrawlerPath = ROOT_DIR + '\\JEXCrawler\\'
 # filename = ['NONTEST.txt']
-filename = []
-for (dirpath, dirnames, filenames) in walk(localpath):
-    filename.extend(filenames)
+filename_jex_crawler = []
+filename_normal_crawler = []
+
+for (dirpath, dirnames, filenames) in walk(NormalCrawlerPath):
+    filename_normal_crawler.extend(filenames)
     break
+
+for (dirpath, dirnames, filenames) in walk(JEXCrawlerPath):
+    filename_jex_crawler.extend(filenames)
+    break
+
 
 cnopts = pysftp.CnOpts(knownhosts='known_hosts')
 now = datetime.datetime.now()
 
 
-with open(config_file) as config_file:
-    config = json.load(config_file)
+def copyFile(filename):
+    for server in config['servers']:
+        try:
+            with pysftp.Connection(host=server['ip'], username=server['username'], private_key=private_key, cnopts=cnopts) as sftp:
+                for file in filename:
+                    check_file = remote_path + file
+                    backup_folder = remote_path + 'backup/'
+                    if sftp.exists(check_file):
+                        if not sftp.exists(backup_folder):
+                            sftp.mkdir(backup_folder)
+                        renamed_file = check_file + str('_') + now.strftime("%Y_%m_%d_%H%M%S")
+                        sftp.rename(check_file, backup_folder + renamed_file.split('/')[-1])
+                        sftp.put(localpath + file, check_file)
+                        print(server['name']+ " : Backed up and coppied new file " + file + " to " + remote_path)
+                    else:
+                        sftp.put(localpath + file, check_file)
+                        print(server['name']+ " : Coppied file " + file + " to " + remote_path)
+            sftp.close()
+                
+        except Exception as e:
+            print(e)
+if __name__ == '__main__':
+    if len(filename_normal_crawler):
+        config_file = config_normal_server_file
+        copyFile(filename_normal_crawler)
 
-for server in config['servers']:
+    if len(filename_jex_crawler):
+        config_file = config_jex_server_file
+        copyFile(filename_jex_crawler)
     try:
-        with pysftp.Connection(host=server['ip'], username=server['username'], private_key=private_key, cnopts=cnopts) as sftp:
-            for file in filename:
-                check_file = remote_path + file
-                backup_folder = remote_path + 'backup/'
-                if sftp.exists(check_file):
-                    if not sftp.exists(backup_folder):
-                        sftp.mkdir(backup_folder)
-                    renamed_file = check_file + str('_') + now.strftime("%Y_%m_%d_%H%M%S")
-                    sftp.rename(check_file, backup_folder + renamed_file.split('/')[-1])
-                    sftp.put(localpath + file, check_file)
-                    print(server['name']+ " : Backed up and coppied new file " + file + " to " + remote_path)
-                else:
-                    sftp.put(localpath + file, check_file)
-                    print(server['name']+ " : Coppied file " + file + " to " + remote_path)
-        sftp.close()
-            
+        if config_file:
+            with open(config_file) as config_file:
+                config = json.load(config_file)
     except Exception as e:
-        print(e)
+        print('Error : ' + str(e))
